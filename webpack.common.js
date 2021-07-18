3d67eadc0487ee688d5c47a58b2e544689b26a0a
nurrizkiadip <66776057+nurrizkiadip@users.noreply.github.com>
2021-07-18 14:28:37 +0700

    submisi3 - finish all criteria submissions 3

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,39 @@
+const path = require('path');
+const ImageminMozjpeg = require('imagemin-mozjpeg');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-const ImageminMozjpeg = require('imagemin-mozjpeg');
-const path = require('path');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
   },
   module: {
     rules: [
@@ -39,7 +63,7 @@ module.exports = {
       template: path.resolve(__dirname, 'src/templates/index.html'),
       filename: 'index.html',
     }),
-    
+
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -66,5 +90,7 @@ module.exports = {
         }),
       ],
     }),
+
+    new BundleAnalyzerPlugin(),
   ],
 };
